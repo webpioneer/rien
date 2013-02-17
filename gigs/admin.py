f66ba60793d3b5e60b4682339f0a36c5757bc0cf
webpioner <webpioner@gmail.com>
2013-02-16 17:09:48 -0800

    Add GigTypeAdmin to gigs/admin.py

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from gigs.models import Category
+from gigs.models import Category, GigType
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date')
-    
+
+class GigTypeAdmin(admin.ModelAdmin):
+    list_display = ('type', 'description', 'price',)
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(GigType, GigTypeAdmin)
