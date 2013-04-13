4caf9cca7d873bc826deebab855a7d5af0032bbf
webpioner <webpioner@gmail.com>
2013-04-12 22:35:02 -0700

    Add find_jobs url pattern to gigs/urls.py

--- gigs/urls.py
+++ gigs/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('gigs.views',
-	url("^$", 'all_gigs', {"template_name": "gigs/index.html"}, name="home"),
+	url("^$", 'all_gigs', {"template_name": "gigs/index.html"}, name = "home"),
+	url("^find_jobs/$", 'find_jobs', name = "find_jobs"),
 	url("^job/(?P<slug>[-\w]+)/$", 'get_gig', {"template_name": "gigs/get_gig.html"}, name="get_gig"),
     url('post_job/$','post_job',{'template_name':'gigs/post_job.html'}, name = 'post_job'),
 )
