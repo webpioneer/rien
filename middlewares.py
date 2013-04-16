489e3a2b85f3dad17a2c246d071534f20df8a02c
webpioner <webpioner@gmail.com>
2013-04-16 16:39:57 -0700

    Update get_language() to return LANGUAGE SETTING from mezzanine.conf.settings instead of hard coding it in a dict

--- middlewares.py
+++ middlewares.py
@@ -1,33 +1,32 @@
 import re
 from django.utils import translation
 
+from mezzanine.conf import settings
 from mezzanine.utils.sites import current_site_id
 
 class LocaleSelectorMiddleware(object):
     "Set the language code for the request depending on what host we're requesting."
 
     #these constants must match what the sites as they are defined in the django_site table
-    LOCALHOST = 1
-    FR = 3
+    #LOCALHOST = 1
+    #FR = 3
 
-    languages = { LOCALHOST: 'en', FR: 'fr', }
+    #languages = { LOCALHOST: 'en', FR: 'fr', }
 
     def process_request(self, req):
         if self.localize_request(req):
             language = self.get_language()
-            print "language :" +language
             translation.activate(language)
             req.LANGUAGE_CODE = translation.get_language()
-            print "req.LANGUAGE_CODE" + req.LANGUAGE_CODE
 
     def get_language(self):
-        sid = current_site_id()
-        if self.languages.get(sid):
-            return self.languages[sid]
-        else:
-            #logger.error("Couldn't get language for site id: %s" % sid)
-            print "Couldn't get language for site id: %s" % sid
-            return 'en'
+        #sid = current_site_id()
+        #if self.languages.get(sid):
+        #    return self.languages[sid]
+        #else:
+        #    logger.error("Couldn't get language for site id: %s" % sid)
+        #    return 'en'
+        return getattr(settings, 'LANGUAGE', 'en')
 
     def localize_request(self, req):
         for no_rewrite_path in ['^/admin/*']:
