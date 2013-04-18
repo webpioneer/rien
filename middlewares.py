3fb48408999ba5fd667337de0ad17493703ae45c
webpioner <webpioner@gmail.com>
2013-04-17 20:36:46 -0700

    Add locales support to LocaleSelectorMiddleware

--- middlewares.py
+++ middlewares.py
@@ -12,12 +12,20 @@ class LocaleSelectorMiddleware(object):
     #FR = 3
 
     #languages = { LOCALHOST: 'en', FR: 'fr', }
+    locales = { 
+        3 : 'fr_FR.UTF-8', # French
+        4 : 'en_GB.UTF-8', # British
+        5 : 'sv_SE.UTF-8', # Swedish
+    }
 
     def process_request(self, req):
         if self.localize_request(req):
             language = self.get_language()
             translation.activate(language)
             req.LANGUAGE_CODE = translation.get_language()
+            # set SHOP_CURRENCY_LOCALE
+            settings.SHOP_CURRENCY_LOCALE =self.locales.get(current_site_id(),'')
+            req.SHOP_CURRENCY_LOCALE = self.locales.get(current_site_id(),'')
 
     def get_language(self):
         #sid = current_site_id()
