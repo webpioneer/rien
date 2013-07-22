0b938f6f3bcf63a0d3f3063c567aa4278dec5edd
webpioner <webpioner@gmail.com>
2013-07-22 04:52:50 -0700

    Add area_level_3 and area_level4

--- static/js/location.js
+++ static/js/location.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
-var geocoder;
+    var country = $('html').attr('lang');
+    console.log(country);
+    var geocoder;
     geocoder=new google.maps.Geocoder();
     var suggestions=[];
     $("#id_location").autocomplete({
         source: function(request,add){
         var suggestions = [];
-        geocoder.geocode( { 'address': request.term}, function(results, status) {                                                             
+        geocoder.geocode( { 'address': request.term, 'region' : country }, function(results, status) {                                                             
             if (status == google.maps.GeocoderStatus.OK){
                 //console.log(results[0]['address_components'],results[0].geometry)
                 for(var i=0;i<results.length;i++){
@@ -22,9 +24,10 @@ var geocoder;
         select : function(e, ui){
             var loc=ui.item.value;
             console.log(loc);
-            geocoder.geocode({'address':loc},function(results,status){                                                      
+
+            geocoder.geocode({'address':loc, 'region' : country },function(results,status){                                                 
                 if(status==google.maps.GeocoderStatus.OK){
-                    
+
                     var longitude=results[0].geometry.location.lng();
                     var latitude=results[0].geometry.location.lat();
                     $("#id_latitude").val(latitude);
@@ -41,6 +44,15 @@ var geocoder;
                         administrative_level_1 = results[0]['address_components'][1].long_name;
                         $("#id_area_level1").val(administrative_level_1);
                     }
+                    if(results[0]['address_components'][3]){
+                        var administrative_level_3 = results[0]['address_components'][3].long_name;
+                        $("#id_area_level3").val(administrative_level_3);
+                    }
+                    if(results[0]['address_components'][4]){
+                        var administrative_level_4 = results[0]['address_components'][4].long_name;
+                        $("#id_area_level4").val(administrative_level_4);
+                    }
+
 
                 }//end if                                                                                                       
         });//end geocode
