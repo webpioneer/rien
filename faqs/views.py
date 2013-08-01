e25434d5b66a64e937b91768baae6caa0303eb39
webpioner <webpioner@gmail.com>
2013-08-01 14:59:57 -0700

    faqs_list returns a list of published FAQs

--- faqs/views.py
+++ faqs/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.http import Http404
+
+from mezzanine.utils.views import render
+
+from faqs.models import Faq
+
+def faqs_list(request, template_name = 'faqs/faqs_list.html'):
+	"""
+	returns a list of published FAQs
+	"""
+	try:
+		faqs = Faq.objects.published()
+	except:
+		raise Http404
+	context = {
+		'faqs' : faqs,
+	}
+	return render(request, template_name, context)
