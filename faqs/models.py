4eb2cfec4243286708bc1759d37b0d663055e806
webpioner <webpioner@gmail.com>
2013-08-01 15:01:38 -0700

    Create Faq Model

--- faqs/models.py
+++ faqs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from mezzanine.core.models import Displayable, Orderable, RichText, Slugged
+
+#from faqs.managers import FaqManager
+
+#class FaqCategory(Orderable, Slugged):
+#	pass
+
+class Faq(Orderable, Displayable, RichText):
+	"""
+	Frequently Asked Question
+	Note : i put Orderable first, then it works
+	"""
+	#categories = models.ManyToManyField(FaqCategory)
+
+	#objects = FaqManager()
+	pass
+
+	def __unicode__(self):
+		return u'Q : {0} - A : {1}'.format(self.title, self.content)
+
