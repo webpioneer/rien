cea8a849ac16596290b2d6714c7044a5431a5684
webpioner <webpioner@gmail.com>
2013-09-18 10:12:43 -0700

    add ChildStatusAdmin class

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from gigs.models import Category, Company, Gig, GigType
+from gigs.models import Category, ChildStatus, Company, Gig, GigType
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date')
@@ -14,7 +14,11 @@ class CompanyAdmin(admin.ModelAdmin):
 class GigAdmin(admin.ModelAdmin):
     list_display = ('job_type', 'title',)
 
+class ChildStatusAdmin(admin.ModelAdmin):
+	list_display = ('name', 'date', 'user')
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(GigType, GigTypeAdmin)
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Gig, GigAdmin)
+admin.site.register(ChildStatus, ChildStatusAdmin)
