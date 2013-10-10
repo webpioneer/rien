e50a4248163a2b0db3ba8adee19b911a5ab1f507
webpioner <webpioner@gmail.com>
2013-10-09 17:44:11 -0700

    Add url patterns to handle feeds

--- marketing/urls.py
+++ marketing/urls.py
@@ -2,7 +2,14 @@ from django.conf.urls.defaults import patterns, url
 
 from mezzanine.core.views import direct_to_template
 
+from marketing.feeds import GigFeed
+
 urlpatterns = patterns('',
 	url("^about/$", direct_to_template, {"template": "marketing/about.html"}, name = "about"),
+	# generate feed (?P<slug>[-\w]+)/
+	url("^jobs/feed/$", GigFeed(), name = 'jobs_feed'),
+	url("^jobs/feed/(?P<what>[-\w]+)/$", GigFeed()),
+	url("^jobs/feed/(?P<what>[-\w]+)/(?P<location>[-\w ]+)/$", GigFeed()),
+	url("^jobs/feed/(?P<what>[-\w]+)/(?P<location>[-\w ]+)/(?P<gig_types>[-\w ]+)/(?P<remote>[-\w ]+)/$", GigFeed()),
 )
 
