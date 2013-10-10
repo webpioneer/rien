015f9b415cfc3a7dadaefd32e09cde62b3c17e98
webpioner <webpioner@gmail.com>
2013-10-09 17:56:06 -0700

    - Add site to list_display - Once gig is validated, gig_is_validated signal is sent

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from gigs.models import Category, ChildStatus, Company, Gig, GigType
+from gigs.signals import gig_is_validated
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'pub_date')
@@ -12,7 +13,13 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = ('type', 'title', 'email',)
 
 class GigAdmin(admin.ModelAdmin):
-    list_display = ('job_type', 'title',)
+    list_display = ('job_type', 'title', 'site')
+
+    def response_change(self, request, obj):
+    	if obj.valid == True:
+    		#send signal
+    		gig_is_validated.send(sender = 'response_change', gig = obj)
+    	return super(GigAdmin, self).response_change(request, obj)
 
 class ChildStatusAdmin(admin.ModelAdmin):
 	list_display = ('name', 'date', 'user')
