d4270bde1d91b0b06ab5e43dd1b4e42701403596
webpioner <webpioner@gmail.com>
2013-10-11 16:23:52 -0700

    Add user and is_onsite to list_display

--- searchapp/admin.py
+++ searchapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from searchapp.models import GigSearch
 
 class GigSearchAdmin(admin.ModelAdmin):
-    list_display = ('what', 'location')
+    list_display = ('user', 'what', 'location', 'is_onsite', )
 
 admin.site.register(GigSearch, GigSearchAdmin)
 
