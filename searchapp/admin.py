fd45cec1fda0bf20af0f3d19ef425adf6673c339
webpioner <webpioner@gmail.com>
2013-10-14 18:25:54 -0700

    Add subscribed_user to GigSearchAdmin list_display

--- searchapp/admin.py
+++ searchapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from searchapp.models import GigSearch
 
 class GigSearchAdmin(admin.ModelAdmin):
-    list_display = ('user', 'what', 'location', 'is_onsite', )
+    list_display = ('user', 'what', 'location', 'is_onsite', 'subscribed_user')
 
 admin.site.register(GigSearch, GigSearchAdmin)
 
